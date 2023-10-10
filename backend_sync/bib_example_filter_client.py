fa4179e43eb65566e30aab1ff5071b1800c7f0d7
Adrien <adrienmontagu@gmail.com>
2023-10-10 15:52:55 +0200

    Just if needed

--- backend_sync/bib_example_filter_client.py
+++ backend_sync/bib_example_filter_client.py
@@ -5,11 +5,11 @@ import json
 from sync_example_bib_app.grpc import sync_example_bib_app_pb2_grpc, sync_example_bib_app_pb2
 
 def main():
-    with grpc.insecure_channel("localhost:50051") as channel:
+    with grpc.insecure_channel("localhost:9082") as channel:
         author_client = sync_example_bib_app_pb2_grpc.AuthorControllerStub(channel)
 
         filter_as_dict = {"name_last": "Doe"}
-        metadata = (("FILTERS", (json.dumps(filter_as_dict))),)
+        metadata = (("filters", (json.dumps(filter_as_dict))),)
 
         response = author_client.List(sync_example_bib_app_pb2.AuthorListRequest(), metadata=metadata)
 
--- backend_sync/bib_example_sync_client.py
+++ backend_sync/bib_example_sync_client.py
@@ -4,7 +4,7 @@ import grpc
 from sync_example_bib_app.grpc import sync_example_bib_app_pb2_grpc, sync_example_bib_app_pb2
 
 def main():
-    with grpc.insecure_channel("localhost:50051") as channel:
+    with grpc.insecure_channel("localhost:9082") as channel:
         stub = sync_example_bib_app_pb2_grpc.AuthorControllerStub(channel)
         
         response = stub.List(sync_example_bib_app_pb2.AuthorListRequest())
