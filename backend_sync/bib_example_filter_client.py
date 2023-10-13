29213627c8936d44a949f4e215c18eefd407701d
mark doerr <mark.doerr@uni-greifswald.de>
2023-10-13 10:18:04 +0200

    fix: changed 'filters' keyword in filter client

--- backend_sync/bib_example_filter_client.py
+++ backend_sync/bib_example_filter_client.py
@@ -9,7 +9,7 @@ def main():
         author_client = sync_example_bib_app_pb2_grpc.AuthorControllerStub(channel)
 
         filter_as_dict = {"name_last": "Doe"}
-        metadata = (("FILTERS", (json.dumps(filter_as_dict))),)
+        metadata = (("filters", (json.dumps(filter_as_dict))),)
 
         response = author_client.List(sync_example_bib_app_pb2.AuthorListRequest(), metadata=metadata)
 
